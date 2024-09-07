799d0de5ec8763110520bedb6dba31b8b40f9669
aliabdulbaqi01 <aliabdulbaqi01@gmail.com>
2024-09-07 08:02:41 +0300

    vendor dashboard

--- app/Http/Controllers/Vendor/VendorController.php
+++ app/Http/Controllers/Vendor/VendorController.php
@@ -8,6 +8,6 @@ use Illuminate\Http\Request;
 class VendorController extends Controller
 {
     public function index() {
-        return view('vendor.index');
+        return view('vendor.dashboard.index');
     }
 }
---
+++ resources/views/vendor/dashboard/layouts/menu.blade.php
@@ -0,0 +1,6 @@
+<div class="wsus__dashboard_menu">
+    <div class="wsusd__dashboard_user">
+        <img src="{{asset('frontend/images/dashboard_user.jpg')}}" alt="img" class="img-fluid">
+        <p>{{auth()->user()->name}}</p>
+    </div>
+</div>
